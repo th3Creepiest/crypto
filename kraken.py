e6e516f87b19d5ee89dd18325d5cbe59df031824
creep <th3creepiest@gmail.com>
2025-03-29 06:43:30 +0100

    more kraken endpoints added

--- kraken.py
+++ kraken.py
@@ -17,3 +17,138 @@ def get_server_time() -> dict:
     response = requests.get(url=BASE_URL + "Time", headers=HEADERS, timeout=10)
     response.raise_for_status()
     return response.json()
+
+
+def get_asset_info(asset: str | None = None, asset_class: str = "currency") -> dict:
+    """Get information about the assets that are available for deposit, withdrawal, trading and earn.
+    asset : string (optional, default: all available assets). Example: XBT,ETH
+    asset_class : string (optional, default: currency)
+    """
+    params = {"aclass": asset_class}
+    if asset:
+        params["asset"] = asset
+
+    response = requests.get(url=BASE_URL + "Assets", headers=HEADERS, params=params, timeout=10)
+    response.raise_for_status()
+    return response.json()
+
+
+def get_tradable_asset_pairs(pair: str | None = None, info: str = "info", country_code: str | None = None) -> dict:
+    """Get tradable asset pairs.
+    pair : string (optional) - Asset pairs to get data for. Example: BTC/USD,ETH/BTC
+    info : string (optional, default: info) - Possible values: [info, leverage, fees, margin]
+    country_code : string (optional) - Filter for response to only include pairs available in provided countries/regions. Example: US,TX,GB,CA
+    """
+    possible_info = ["info", "leverage", "fees", "margin"]
+    if info not in possible_info:
+        raise ValueError(f"Invalid info value. Possible values: {possible_info}")
+
+    params = {"info": info}
+
+    if pair:
+        params["pair"] = pair
+
+    if country_code:
+        params["country_code"] = country_code
+
+    response = requests.get(url=BASE_URL + "AssetPairs", headers=HEADERS, params=params, timeout=10)
+    response.raise_for_status()
+    return response.json()
+
+
+def get_ticker_information(pair: str | None = None) -> dict:
+    """Get ticker information for all or requested markets.
+
+    To clarify usage, note that:
+        * Today's prices start at midnight UTC
+        * Leaving the pair parameter blank will return tickers for all tradeable assets on Kraken
+
+    pair: string - Asset pair to get data for (optional, default: all tradeable exchange pairs) - Example: XBTUSD
+    """
+    params = {}
+    if pair:
+        params["pair"] = pair
+
+    response = requests.get(url=BASE_URL + "Ticker", headers=HEADERS, params=params, timeout=10)
+    response.raise_for_status()
+    return response.json()
+
+
+def get_ohlc_data(pair: str | None = None, interval: int = 60, since: int | None = None) -> dict:
+    """Retrieve OHLC market data.
+
+    The last entry in the OHLC array is for the current, not-yet-committed timeframe, and will always be present, regardless of the value of since.
+    Returns up to 720 of the most recent entries (older data cannot be retrieved, regardless of the value of since).
+
+    pair: string - Asset pair to get data for (optional, default: all tradeable exchange pairs) - Example: XBTUSD
+    interval: integer - Time frame interval in minutes (optional, default: 1) - Possible values: 1, 5, 15, 30, 60, 240, 1440, 10080, 21600
+    since: integer - Return only data from this timestamp onwards (optional)
+    """
+    possible_intervals = [1, 5, 15, 30, 60, 240, 1440, 10080, 21600]
+    if interval not in possible_intervals:
+        raise ValueError(f"Invalid interval value. Possible values: {possible_intervals}")
+
+    params = {}
+
+    if pair:
+        params["pair"] = pair
+
+    if since:
+        params["since"] = since
+
+    params["interval"] = interval
+
+    response = requests.get(url=BASE_URL + "OHLC", headers=HEADERS, params=params, timeout=10)
+    response.raise_for_status()
+    return response.json()
+
+
+def get_order_book(pair: str, count: int = 100) -> dict:
+    """Returns level 2 (L2) order book, which describes the individual price levels in the book with aggregated order quantities at each level.
+    pair: string - Asset pair to get data for (required) - Example: XBTUSD
+    count: integer - Possible values: >= 1 and <= 500 - Default value: 100 - Maximum number of asks/bids
+    """
+    if count < 1 or count > 500:
+        raise ValueError("Count must be between 1 and 500")
+
+    params = {"pair": pair, "count": count}
+
+    response = requests.get(url=BASE_URL + "Depth", headers=HEADERS, params=params, timeout=10)
+    response.raise_for_status()
+    return response.json()
+
+
+def get_recent_trades(pair: str, count: int = 1000, since: int | None = None) -> dict:
+    """Returns the last 1000 trades by default.
+    pair: string - Asset pair to get data for (required) - Example: XBTUSD
+    since: string - Return trade data since given timestamp (optional) - Example: 1616663618
+    count: integer - Possible values: >= 1 and <= 1000 - Default value: 1000 - Return specific number of trades, up to 1000
+    """
+    if count < 1 or count > 1000:
+        raise ValueError("Count must be between 1 and 1000")
+
+    params = {"pair": pair, "count": count}
+
+    if since:
+        params["since"] = since
+
+    response = requests.get(url=BASE_URL + "Trades", headers=HEADERS, params=params, timeout=10)
+    response.raise_for_status()
+    return response.json()
+
+
+def get_recent_spreads(pair: str, since: int | None = None) -> dict:
+    """Returns the last ~200 top-of-book spreads for a given pair.
+    pair: string - Asset pair to get data for (required) - Example: XBTUSD
+    since: integer - Returns spread data since given timestamp. Optional, intended for incremental updates within available dataset (does not contain all historical spreads).
+        * Example: 1678219570
+    """
+    params = {}
+    params["pair"] = pair
+
+    if since:
+        params["since"] = since
+
+    response = requests.get(url=BASE_URL + "Spread", headers=HEADERS, params=params, timeout=10)
+    response.raise_for_status()
+    return response.json()
--- kraken_test.py
+++ kraken_test.py
@@ -1,7 +1,17 @@
 import pytest
 from requests import HTTPError
 
-from kraken import get_system_status, get_server_time
+from kraken import (
+    get_system_status,
+    get_server_time,
+    get_asset_info,
+    get_tradable_asset_pairs,
+    get_ticker_information,
+    get_ohlc_data,
+    get_order_book,
+    get_recent_trades,
+    get_recent_spreads,
+)
 
 
 HEADERS = {"User-Agent": "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/56.0.2924.87 Safari/537.36", "Accept-Encoding": "gzip, deflate", "Accept": "application/json", "Connection": "keep-alive"}
@@ -63,5 +73,271 @@ class TestGetServerTime:
             get_server_time()
         assert requests_mock.called_once
         assert requests_mock.last_request.url == self.URL
+        assert requests_mock.last_request.method == "GET"
+        assert requests_mock.last_request.headers == HEADERS
+
+
+class TestGetAssetInfo:
+    URL = "https://api.kraken.com/0/public/Assets"
+
+    def test_get_asset_info_success(self, requests_mock):
+        requests_mock.get(self.URL, json={})
+        assert get_asset_info() == {}
+        assert requests_mock.called_once
+        assert requests_mock.last_request.url == self.URL + "?aclass=currency"
+        assert requests_mock.last_request.method == "GET"
+        assert requests_mock.last_request.headers == HEADERS
+
+    def test_get_asset_info_with_asset_success(self, requests_mock):
+        requests_mock.get(self.URL, json={})
+        assert get_asset_info(asset="XBT") == {}
+        assert requests_mock.called_once
+        assert requests_mock.last_request.url == self.URL + "?aclass=currency&asset=XBT"
+        assert requests_mock.last_request.method == "GET"
+        assert requests_mock.last_request.headers == HEADERS
+
+    @pytest.mark.parametrize(
+        "status_code",
+        [400, 403, 500],
+        ids=[
+            "test_get_asset_info_failure: Bad Request",
+            "test_get_asset_info_failure: Forbidden",
+            "test_get_asset_info_failure: Internal Server Error",
+        ],
+    )
+    def test_get_asset_info_failure(self, requests_mock, status_code):
+        requests_mock.get(self.URL, status_code=status_code)
+        with pytest.raises(HTTPError):
+            get_asset_info()
+        assert requests_mock.called_once
+        assert requests_mock.last_request.url == self.URL + "?aclass=currency"
+        assert requests_mock.last_request.method == "GET"
+        assert requests_mock.last_request.headers == HEADERS
+
+
+class TestGetTradableAssetPairs:
+    URL = "https://api.kraken.com/0/public/AssetPairs"
+
+    def test_get_tradable_asset_pairs_success(self, requests_mock):
+        requests_mock.get(self.URL, json={})
+        assert get_tradable_asset_pairs() == {}
+        assert requests_mock.called_once
+        assert requests_mock.last_request.url == self.URL + "?info=info"
+        assert requests_mock.last_request.method == "GET"
+        assert requests_mock.last_request.headers == HEADERS
+
+    def test_get_tradable_asset_pairs_with_pair_success(self, requests_mock):
+        requests_mock.get(self.URL, json={})
+        assert get_tradable_asset_pairs(pair="BTC/USD") == {}
+        assert requests_mock.called_once
+        assert requests_mock.last_request.url == self.URL + "?info=info&pair=BTC%2FUSD"
+        assert requests_mock.last_request.method == "GET"
+        assert requests_mock.last_request.headers == HEADERS
+
+    def test_get_tradable_asset_pairs_with_country_code_success(self, requests_mock):
+        requests_mock.get(self.URL, json={})
+        assert get_tradable_asset_pairs(country_code="US") == {}
+        assert requests_mock.called_once
+        assert requests_mock.last_request.url == self.URL + "?info=info&country_code=US"
+        assert requests_mock.last_request.method == "GET"
+        assert requests_mock.last_request.headers == HEADERS
+
+    @pytest.mark.parametrize(
+        "status_code",
+        [400, 403, 500],
+        ids=[
+            "test_get_tradable_asset_pairs_failure: Bad Request",
+            "test_get_tradable_asset_pairs_failure: Forbidden",
+            "test_get_tradable_asset_pairs_failure: Internal Server Error",
+        ],
+    )
+    def test_get_tradable_asset_pairs_failure(self, requests_mock, status_code):
+        requests_mock.get(self.URL, status_code=status_code)
+        with pytest.raises(HTTPError):
+            get_tradable_asset_pairs()
+        assert requests_mock.called_once
+        assert requests_mock.last_request.url == self.URL + "?info=info"
+        assert requests_mock.last_request.method == "GET"
+        assert requests_mock.last_request.headers == HEADERS
+
+
+class TestGetTickerInformation:
+    URL = "https://api.kraken.com/0/public/Ticker"
+
+    def test_get_ticker_information_success(self, requests_mock):
+        requests_mock.get(self.URL, json={})
+        assert get_ticker_information() == {}
+        assert requests_mock.called_once
+        assert requests_mock.last_request.url == self.URL
+        assert requests_mock.last_request.method == "GET"
+        assert requests_mock.last_request.headers == HEADERS
+
+    def test_get_ticker_information_with_pair_success(self, requests_mock):
+        requests_mock.get(self.URL, json={})
+        assert get_ticker_information(pair="BTC/USD") == {}
+        assert requests_mock.called_once
+        assert requests_mock.last_request.url == self.URL + "?pair=BTC%2FUSD"
+        assert requests_mock.last_request.method == "GET"
+        assert requests_mock.last_request.headers == HEADERS
+
+    @pytest.mark.parametrize(
+        "status_code",
+        [400, 403, 500],
+        ids=[
+            "test_get_ticker_information_failure: Bad Request",
+            "test_get_ticker_information_failure: Forbidden",
+            "test_get_ticker_information_failure: Internal Server Error",
+        ],
+    )
+    def test_get_ticker_information_failure(self, requests_mock, status_code):
+        requests_mock.get(self.URL, status_code=status_code)
+        with pytest.raises(HTTPError):
+            get_ticker_information()
+        assert requests_mock.called_once
+        assert requests_mock.last_request.url == self.URL
+        assert requests_mock.last_request.method == "GET"
+        assert requests_mock.last_request.headers == HEADERS
+
+
+class TestGetOhlcData:
+    URL = "https://api.kraken.com/0/public/OHLC"
+
+    def test_get_ohlc_data_success(self, requests_mock):
+        requests_mock.get(self.URL, json={})
+        assert get_ohlc_data() == {}
+        assert requests_mock.called_once
+        assert requests_mock.last_request.url == self.URL + "?interval=60"
+        assert requests_mock.last_request.method == "GET"
+        assert requests_mock.last_request.headers == HEADERS
+
+    def test_get_ohlc_data_with_pair_success(self, requests_mock):
+        requests_mock.get(self.URL, json={})
+        assert get_ohlc_data(pair="BTC/USD") == {}
+        assert requests_mock.called_once
+        assert requests_mock.last_request.url == self.URL + "?pair=BTC%2FUSD&interval=60"
+        assert requests_mock.last_request.method == "GET"
+        assert requests_mock.last_request.headers == HEADERS
+
+    @pytest.mark.parametrize(
+        "status_code",
+        [400, 403, 500],
+        ids=[
+            "test_get_ohlc_data_failure: Bad Request",
+            "test_get_ohlc_data_failure: Forbidden",
+            "test_get_ohlc_data_failure: Internal Server Error",
+        ],
+    )
+    def test_get_ohlc_data_failure(self, requests_mock, status_code):
+        requests_mock.get(self.URL, status_code=status_code)
+        with pytest.raises(HTTPError):
+            get_ohlc_data()
+        assert requests_mock.called_once
+        assert requests_mock.last_request.url == self.URL + "?interval=60"
+        assert requests_mock.last_request.method == "GET"
+        assert requests_mock.last_request.headers == HEADERS
+
+
+class TestGetOrderBook:
+    URL = "https://api.kraken.com/0/public/Depth"
+
+    def test_get_order_book_success(self, requests_mock):
+        requests_mock.get(self.URL, json={})
+        assert get_order_book(pair="BTC/USD") == {}
+        assert requests_mock.called_once
+        assert requests_mock.last_request.url == self.URL + "?pair=BTC%2FUSD&count=100"
+        assert requests_mock.last_request.method == "GET"
+        assert requests_mock.last_request.headers == HEADERS
+
+    @pytest.mark.parametrize(
+        "status_code",
+        [400, 403, 500],
+        ids=[
+            "test_get_order_book_failure: Bad Request",
+            "test_get_order_book_failure: Forbidden",
+            "test_get_order_book_failure: Internal Server Error",
+        ],
+    )
+    def test_get_order_book_failure(self, requests_mock, status_code):
+        requests_mock.get(self.URL, status_code=status_code)
+        with pytest.raises(HTTPError):
+            get_order_book(pair="BTC/USD")
+        assert requests_mock.called_once
+        assert requests_mock.last_request.url == self.URL + "?pair=BTC%2FUSD&count=100"
+        assert requests_mock.last_request.method == "GET"
+        assert requests_mock.last_request.headers == HEADERS
+
+
+class TestGetRecentTrades:
+    URL = "https://api.kraken.com/0/public/Trades"
+
+    def test_get_recent_trades_success(self, requests_mock):
+        requests_mock.get(self.URL, json={})
+        assert get_recent_trades(pair="BTC/USD") == {}
+        assert requests_mock.called_once
+        assert requests_mock.last_request.url == self.URL + "?pair=BTC%2FUSD&count=1000"
+        assert requests_mock.last_request.method == "GET"
+        assert requests_mock.last_request.headers == HEADERS
+
+    def test_get_recent_trades_since(self, requests_mock):
+        requests_mock.get(self.URL, json={})
+        assert get_recent_trades(pair="BTC/USD", since=1616663618) == {}
+        assert requests_mock.called_once
+        assert requests_mock.last_request.url == self.URL + "?pair=BTC%2FUSD&count=1000&since=1616663618"
+        assert requests_mock.last_request.method == "GET"
+        assert requests_mock.last_request.headers == HEADERS
+
+    @pytest.mark.parametrize(
+        "status_code",
+        [400, 403, 500],
+        ids=[
+            "test_get_recent_trades_failure: Bad Request",
+            "test_get_recent_trades_failure: Forbidden",
+            "test_get_recent_trades_failure: Internal Server Error",
+        ],
+    )
+    def test_get_recent_trades_failure(self, requests_mock, status_code):
+        requests_mock.get(self.URL, status_code=status_code)
+        with pytest.raises(HTTPError):
+            get_recent_trades(pair="BTC/USD")
+        assert requests_mock.called_once
+        assert requests_mock.last_request.url == self.URL + "?pair=BTC%2FUSD&count=1000"
+        assert requests_mock.last_request.method == "GET"
+        assert requests_mock.last_request.headers == HEADERS
+
+
+class TestGetRecentSpreads:
+    URL = "https://api.kraken.com/0/public/Spread"
+
+    def test_get_recent_spreads_success(self, requests_mock):
+        requests_mock.get(self.URL, json={})
+        assert get_recent_spreads(pair="BTC/USD") == {}
+        assert requests_mock.called_once
+        assert requests_mock.last_request.url == self.URL + "?pair=BTC%2FUSD"
+        assert requests_mock.last_request.method == "GET"
+        assert requests_mock.last_request.headers == HEADERS
+
+    def test_get_recent_spreads_since(self, requests_mock):
+        requests_mock.get(self.URL, json={})
+        assert get_recent_spreads(pair="BTC/USD", since=1616663618) == {}
+        assert requests_mock.called_once
+        assert requests_mock.last_request.url == self.URL + "?pair=BTC%2FUSD&since=1616663618"
+        assert requests_mock.last_request.method == "GET"
+        assert requests_mock.last_request.headers == HEADERS
+
+    @pytest.mark.parametrize(
+        "status_code",
+        [400, 403, 500],
+        ids=[
+            "test_get_recent_spreads_failure: Bad Request",
+            "test_get_recent_spreads_failure: Forbidden",
+            "test_get_recent_spreads_failure: Internal Server Error",
+        ],
+    )
+    def test_get_recent_spreads_failure(self, requests_mock, status_code):
+        requests_mock.get(self.URL, status_code=status_code)
+        with pytest.raises(HTTPError):
+            get_recent_spreads(pair="BTC/USD")
+        assert requests_mock.called_once
+        assert requests_mock.last_request.url == self.URL + "?pair=BTC%2FUSD"
         assert requests_mock.last_request.method == "GET"
         assert requests_mock.last_request.headers == HEADERS
