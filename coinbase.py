2608eb0ba87fabbcd0982b51d96ebece3e11d5ef
creep <th3creepiest@gmail.com>
2025-03-29 11:52:38 +0100

    more coinbase endpoints added

--- coinbase.py
+++ coinbase.py
@@ -1,12 +1,130 @@
 import requests
 
 
-BASE_URL = "https://api.coinbase.com/v2/"
-HEADERS = {"User-Agent": "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/56.0.2924.87 Safari/537.36", "Accept": "application/json"}
+HEADERS = {"User-Agent": "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/56.0.2924.87 Safari/537.36", "Accept-Encoding": "gzip, deflate", "Accept": "application/json", "Connection": "keep-alive"}
 
 
 def get_server_time() -> dict:
     """Test connectivity to the Rest API and get the current server time."""
-    response = requests.get(url=BASE_URL + "time", headers=HEADERS, timeout=10)
+    response = requests.get(url="https://api.coinbase.com/v2/time", headers=HEADERS, timeout=10)
+    response.raise_for_status()
+    return response.json()
+
+
+def list_currencies() -> dict:
+    """Get a list of all known currencies."""
+    response = requests.get(url="https://api.coinbase.com/v2/currencies", headers=HEADERS, timeout=10)
+    response.raise_for_status()
+    return response.json()
+
+
+def get_currency(currency_id: str) -> dict:
+    """Get a currency by ID."""
+    params = {"currency_id": currency_id}
+    response = requests.get(url="https://api.coinbase.com/v2/currencies", headers=HEADERS, params=params, timeout=10)
+    response.raise_for_status()
+    return response.json()
+
+
+def get_single_product_pairs(product_id: str) -> dict:
+    """Get a list of available currency pairs for trading."""
+    params = {"product_id": product_id}
+    response = requests.get(url="https://api.coinbase.com/v2/products", headers=HEADERS, params=params, timeout=10)
+    response.raise_for_status()
+    return response.json()
+
+
+def list_trading_pairs() -> dict:
+    """Get a list of available currency pairs for trading."""
+    response = requests.get(url="https://api.exchange.coinbase.com/products", headers=HEADERS, timeout=10)
+    response.raise_for_status()
+    return response.json()
+
+
+def get_all_product_volume() -> dict:
+    """Gets 30day and 24hour volume for all products and market types."""
+    response = requests.get(url="https://api.exchange.coinbase.com/products/volume-summary", headers=HEADERS, timeout=10)
+    response.raise_for_status()
+    return response.json()
+
+
+def get_single_product_info(product_id: str) -> dict:
+    """Get information on a single product."""
+    params = {"product_id": product_id}
+    response = requests.get(url="https://api.exchange.coinbase.com/products", headers=HEADERS, params=params, timeout=10)
+    response.raise_for_status()
+    return response.json()
+
+
+def get_product_candles(product_id: str, granularity: int | None = None, start: str | None = None, end: str | None = None) -> dict:
+    """Historic rates for a product.
+    Rates are returned in grouped buckets.
+    Candle schema is of the form [timestamp, price_low, price_high, price_open, price_close]
+
+    Parameters
+    ----------
+    product_id : str
+        The product ID. Example: "BTC-USD"
+    granularity : int | None
+        The granularity field must be one of the following "second" values: {60, 300, 900, 3600, 21600, 86400}, or your request is rejected.
+        These values correspond to time-slices representing one minute, five minutes, fifteen minutes, one hour, six hours, and one day, respectively.
+    start : str | None
+        Timestamp for starting range of aggregations.
+    end : str | None
+        Timestamp for ending range of aggregations.
+
+    Returns
+    -------
+    dict
+        A dictionary of the candles.
+
+    Max Candles
+        The maximum number of data points for a single request is 300 candles.
+        If your selection of start/end time and granularity results in more than 300 data points, your request is rejected.
+        To retrieve fine granularity data over a larger time range, you must make multiple requests with new start/end ranges.
+    """
+
+    if granularity and granularity not in [60, 300, 900, 3600, 21600, 86400]:
+        raise ValueError("Granularity must be 60, 300, 900, 3600, 21600, or 86400")
+
+    params = {}
+
+    if granularity:
+        params["granularity"] = granularity
+
+    if start:
+        params["start"] = start
+
+    if end:
+        params["end"] = end
+
+    response = requests.get(url=f"https://api.exchange.coinbase.com/products/{product_id}/candles", headers=HEADERS, params=params, timeout=10)
+    response.raise_for_status()
+    return response.json()
+
+
+def get_product_stats(product_id: str) -> dict:
+    """Gets 30day and 24hour stats for a product.
+    product_id : str - Example: "BTC-USD"
+    """
+    response = requests.get(url=f"https://api.exchange.coinbase.com/products/{product_id}/stats", headers=HEADERS, timeout=10)
+    response.raise_for_status()
+    return response.json()
+
+
+def get_product_ticker(product_id: str) -> dict:
+    """Gets snapshot information about the last trade (tick), best bid/ask and 24h volume.
+    product_id : str - Example: "BTC-USD"
+    """
+    response = requests.get(url=f"https://api.exchange.coinbase.com/products/{product_id}/ticker", headers=HEADERS, timeout=10)
+    response.raise_for_status()
+    return response.json()
+
+
+def get_product_trades(product_id: str) -> dict:
+    """Gets a list of the latest trades for a product.
+    product_id : str - Example: "BTC-USD"
+    """
+    response = requests.get(url=f"https://api.exchange.coinbase.com/products/{product_id}/trades", headers=HEADERS, timeout=10)
     response.raise_for_status()
     return response.json()
--- coinbase_test.py
+++ coinbase_test.py
@@ -1,7 +1,19 @@
 import pytest
 from requests import HTTPError
 
-from coinbase import get_server_time
+from coinbase import (
+    get_server_time,
+    list_currencies,
+    get_currency,
+    get_single_product_pairs,
+    list_trading_pairs,
+    get_all_product_volume,
+    get_single_product_info,
+    get_product_candles,
+    get_product_stats,
+    get_product_ticker,
+    get_product_trades,
+)
 
 
 HEADERS = {"User-Agent": "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/56.0.2924.87 Safari/537.36", "Accept-Encoding": "gzip, deflate", "Accept": "application/json", "Connection": "keep-alive"}
@@ -33,5 +45,327 @@ class TestGetServerTime:
             get_server_time()
         assert requests_mock.called_once
         assert requests_mock.last_request.url == self.URL
+        assert requests_mock.last_request.method == "GET"
+        assert requests_mock.last_request.headers == HEADERS
+
+
+class TestListCurrencies:
+    URL = "https://api.coinbase.com/v2/currencies"
+
+    def test_list_currencies_success(self, requests_mock):
+        requests_mock.get(self.URL, json={})
+        assert list_currencies() == {}
+        assert requests_mock.called_once
+        assert requests_mock.last_request.url == self.URL
+        assert requests_mock.last_request.method == "GET"
+        assert requests_mock.last_request.headers == HEADERS
+
+    @pytest.mark.parametrize(
+        "status_code",
+        [400, 403, 500],
+        ids=[
+            "test_list_currencies_failure: Bad Request",
+            "test_list_currencies_failure: Forbidden",
+            "test_list_currencies_failure: Internal Server Error",
+        ],
+    )
+    def test_list_currencies_failure(self, requests_mock, status_code):
+        requests_mock.get(self.URL, status_code=status_code)
+        with pytest.raises(HTTPError):
+            list_currencies()
+        assert requests_mock.called_once
+        assert requests_mock.last_request.url == self.URL
+        assert requests_mock.last_request.method == "GET"
+        assert requests_mock.last_request.headers == HEADERS
+
+
+class TestGetCurrency:
+    URL = "https://api.coinbase.com/v2/currencies"
+
+    def test_get_currency_success(self, requests_mock):
+        requests_mock.get(self.URL, json={})
+        assert get_currency("EUR") == {}
+        assert requests_mock.called_once
+        assert requests_mock.last_request.url == self.URL + "?currency_id=EUR"
+        assert requests_mock.last_request.method == "GET"
+        assert requests_mock.last_request.headers == HEADERS
+
+    @pytest.mark.parametrize(
+        "status_code",
+        [400, 403, 500],
+        ids=[
+            "test_get_currency_failure: Bad Request",
+            "test_get_currency_failure: Forbidden",
+            "test_get_currency_failure: Internal Server Error",
+        ],
+    )
+    def test_get_currency_failure(self, requests_mock, status_code):
+        requests_mock.get(self.URL, status_code=status_code)
+        with pytest.raises(HTTPError):
+            get_currency("USD")
+        assert requests_mock.called_once
+        assert requests_mock.last_request.url == self.URL + "?currency_id=USD"
+        assert requests_mock.last_request.method == "GET"
+        assert requests_mock.last_request.headers == HEADERS
+
+
+class TestGetSingleProductPairs:
+    URL = "https://api.coinbase.com/v2/products"
+
+    def test_get_single_product_pairs_success(self, requests_mock):
+        requests_mock.get(self.URL, json=[])
+        assert get_single_product_pairs("BTC-USD") == []
+        assert requests_mock.called_once
+        assert requests_mock.last_request.url == self.URL + "?product_id=BTC-USD"
+        assert requests_mock.last_request.method == "GET"
+        assert requests_mock.last_request.headers == HEADERS
+
+    @pytest.mark.parametrize(
+        "status_code",
+        [400, 403, 500],
+        ids=[
+            "test_get_single_product_pairs_failure: Bad Request",
+            "test_get_single_product_pairs_failure: Forbidden",
+            "test_get_single_product_pairs_failure: Internal Server Error",
+        ],
+    )
+    def test_get_single_product_pairs_failure(self, requests_mock, status_code):
+        requests_mock.get(self.URL, status_code=status_code)
+        with pytest.raises(HTTPError):
+            get_single_product_pairs("BTC-USD")
+        assert requests_mock.called_once
+        assert requests_mock.last_request.url == self.URL + "?product_id=BTC-USD"
+        assert requests_mock.last_request.method == "GET"
+        assert requests_mock.last_request.headers == HEADERS
+
+
+class TestListTradingPairs:
+    URL = "https://api.exchange.coinbase.com/products"
+
+    def test_list_trading_pairs_success(self, requests_mock):
+        requests_mock.get(self.URL, json=[])
+        assert list_trading_pairs() == []
+        assert requests_mock.called_once
+        assert requests_mock.last_request.url == self.URL
+        assert requests_mock.last_request.method == "GET"
+        assert requests_mock.last_request.headers == HEADERS
+
+    @pytest.mark.parametrize(
+        "status_code",
+        [400, 403, 500],
+        ids=[
+            "test_list_trading_pairs_failure: Bad Request",
+            "test_list_trading_pairs_failure: Forbidden",
+            "test_list_trading_pairs_failure: Internal Server Error",
+        ],
+    )
+    def test_list_trading_pairs_failure(self, requests_mock, status_code):
+        requests_mock.get(self.URL, status_code=status_code)
+        with pytest.raises(HTTPError):
+            list_trading_pairs()
+        assert requests_mock.called_once
+        assert requests_mock.last_request.url == self.URL
+        assert requests_mock.last_request.method == "GET"
+        assert requests_mock.last_request.headers == HEADERS
+
+
+class TestGetAllProductVolume:
+    URL = "https://api.exchange.coinbase.com/products/volume-summary"
+
+    def test_get_all_product_volume_success(self, requests_mock):
+        requests_mock.get(self.URL, json=[])
+        assert get_all_product_volume() == []
+        assert requests_mock.called_once
+        assert requests_mock.last_request.url == self.URL
+        assert requests_mock.last_request.method == "GET"
+        assert requests_mock.last_request.headers == HEADERS
+
+    @pytest.mark.parametrize(
+        "status_code",
+        [400, 403, 500],
+        ids=[
+            "test_get_all_product_volume_failure: Bad Request",
+            "test_get_all_product_volume_failure: Forbidden",
+            "test_get_all_product_volume_failure: Internal Server Error",
+        ],
+    )
+    def test_get_all_product_volume_failure(self, requests_mock, status_code):
+        requests_mock.get(self.URL, status_code=status_code)
+        with pytest.raises(HTTPError):
+            get_all_product_volume()
+        assert requests_mock.called_once
+        assert requests_mock.last_request.url == self.URL
+        assert requests_mock.last_request.method == "GET"
+        assert requests_mock.last_request.headers == HEADERS
+
+
+class TestGetSingleProductVolume:
+    URL = "https://api.exchange.coinbase.com/products"
+
+    def test_get_single_product_volume_success(self, requests_mock):
+        requests_mock.get(self.URL, json=[])
+        assert get_single_product_info("BTC-USD") == []
+        assert requests_mock.called_once
+        assert requests_mock.last_request.url == self.URL + "?product_id=BTC-USD"
+        assert requests_mock.last_request.method == "GET"
+        assert requests_mock.last_request.headers == HEADERS
+
+    @pytest.mark.parametrize(
+        "status_code",
+        [400, 403, 500],
+        ids=[
+            "test_get_single_product_volume_failure: Bad Request",
+            "test_get_single_product_volume_failure: Forbidden",
+            "test_get_single_product_volume_failure: Internal Server Error",
+        ],
+    )
+    def test_get_single_product_volume_failure(self, requests_mock, status_code):
+        requests_mock.get(self.URL, status_code=status_code)
+        with pytest.raises(HTTPError):
+            get_single_product_info("BTC-USD")
+        assert requests_mock.called_once
+        assert requests_mock.last_request.url == self.URL + "?product_id=BTC-USD"
+        assert requests_mock.last_request.method == "GET"
+        assert requests_mock.last_request.headers == HEADERS
+
+
+class TestGetProductCandles:
+    URL = "https://api.exchange.coinbase.com/products"
+
+    def test_get_product_candles_success(self, requests_mock):
+        requests_mock.get(self.URL + "/BTC-USD/candles", json={})
+        assert get_product_candles("BTC-USD") == {}
+        assert requests_mock.called_once
+        assert requests_mock.last_request.url == self.URL + "/BTC-USD/candles"
+        assert requests_mock.last_request.method == "GET"
+        assert requests_mock.last_request.headers == HEADERS
+
+    def test_get_product_candles_with_granularity_success(self, requests_mock):
+        requests_mock.get(self.URL + "/BTC-USD/candles", json={})
+        assert get_product_candles("BTC-USD", granularity=60) == {}
+        assert requests_mock.called_once
+        assert requests_mock.last_request.url == self.URL + "/BTC-USD/candles?granularity=60"
+        assert requests_mock.last_request.method == "GET"
+        assert requests_mock.last_request.headers == HEADERS
+
+    @pytest.mark.parametrize(
+        "status_code",
+        [400, 403, 500],
+        ids=[
+            "test_get_product_candles_failure: Bad Request",
+            "test_get_product_candles_failure: Forbidden",
+            "test_get_product_candles_failure: Internal Server Error",
+        ],
+    )
+    def test_get_product_candles_failure(self, requests_mock, status_code):
+        requests_mock.get(self.URL + "/BTC-USD/candles", status_code=status_code)
+        with pytest.raises(HTTPError):
+            get_product_candles("BTC-USD")
+        assert requests_mock.called_once
+        assert requests_mock.last_request.url == self.URL + "/BTC-USD/candles"
+        assert requests_mock.last_request.method == "GET"
+        assert requests_mock.last_request.headers == HEADERS
+
+
+class TestGetProductStats:
+    URL = "https://api.exchange.coinbase.com/products"
+
+    def test_get_product_stats_success(self, requests_mock):
+        requests_mock.get(
+            self.URL + "/BTC/stats",
+            json={"open": "84927.78", "high": "85562.07", "low": "83217.26", "last": "83233.97", "volume": "8043.06336134", "volume_30day": "336320.45431582", "rfq_volume_24hour": "40.724795", "rfq_volume_30day": "3146.264482"},
+        )
+        assert get_product_stats("BTC") == {
+            "open": "84927.78",
+            "high": "85562.07",
+            "low": "83217.26",
+            "last": "83233.97",
+            "volume": "8043.06336134",
+            "volume_30day": "336320.45431582",
+            "rfq_volume_24hour": "40.724795",
+            "rfq_volume_30day": "3146.264482",
+        }
+        assert requests_mock.called_once
+        assert requests_mock.last_request.url == self.URL + "/BTC/stats"
+        assert requests_mock.last_request.method == "GET"
+        assert requests_mock.last_request.headers == HEADERS
+
+    @pytest.mark.parametrize(
+        "status_code",
+        [400, 403, 500],
+        ids=[
+            "test_get_product_stats_failure: Bad Request",
+            "test_get_product_stats_failure: Forbidden",
+            "test_get_product_stats_failure: Internal Server Error",
+        ],
+    )
+    def test_get_product_stats_failure(self, requests_mock, status_code):
+        requests_mock.get(self.URL + "/BTC/stats", status_code=status_code)
+        with pytest.raises(HTTPError):
+            get_product_stats("BTC")
+        assert requests_mock.called_once
+        assert requests_mock.last_request.url == self.URL + "/BTC/stats"
+        assert requests_mock.last_request.method == "GET"
+        assert requests_mock.last_request.headers == HEADERS
+
+
+class TestGetProductTicker:
+    URL = "https://api.exchange.coinbase.com/products"
+
+    def test_get_product_ticker_success(self, requests_mock):
+        requests_mock.get(
+            self.URL + "/BTC-USD/ticker", json={"ask": "83127.85", "bid": "83127.84", "volume": "8059.69043853", "trade_id": 803279920, "price": "83127.85", "size": "0.00040389", "time": "2025-03-29T10:49:07.210762Z", "rfq_volume": "40.699221"}
+        )
+        assert get_product_ticker("BTC-USD") == {"ask": "83127.85", "bid": "83127.84", "volume": "8059.69043853", "trade_id": 803279920, "price": "83127.85", "size": "0.00040389", "time": "2025-03-29T10:49:07.210762Z", "rfq_volume": "40.699221"}
+        assert requests_mock.called_once
+        assert requests_mock.last_request.url == self.URL + "/BTC-USD/ticker"
+        assert requests_mock.last_request.method == "GET"
+        assert requests_mock.last_request.headers == HEADERS
+
+    @pytest.mark.parametrize(
+        "status_code",
+        [400, 403, 500],
+        ids=[
+            "test_get_product_ticker_failure: Bad Request",
+            "test_get_product_ticker_failure: Forbidden",
+            "test_get_product_ticker_failure: Internal Server Error",
+        ],
+    )
+    def test_get_product_ticker_failure(self, requests_mock, status_code):
+        requests_mock.get(self.URL + "/BTC-USD/ticker", status_code=status_code)
+        with pytest.raises(HTTPError):
+            get_product_ticker("BTC-USD")
+        assert requests_mock.called_once
+        assert requests_mock.last_request.url == self.URL + "/BTC-USD/ticker"
+        assert requests_mock.last_request.method == "GET"
+        assert requests_mock.last_request.headers == HEADERS
+
+
+class TestGetProductTrades:
+    URL = "https://api.exchange.coinbase.com/products"
+
+    def test_get_product_trades_success(self, requests_mock):
+        requests_mock.get(self.URL + "/BTC-USD/trades", json=[])
+        assert get_product_trades("BTC-USD") == []
+        assert requests_mock.called_once
+        assert requests_mock.last_request.url == self.URL + "/BTC-USD/trades"
+        assert requests_mock.last_request.method == "GET"
+        assert requests_mock.last_request.headers == HEADERS
+
+    @pytest.mark.parametrize(
+        "status_code",
+        [400, 403, 500],
+        ids=[
+            "test_get_product_trades_failure: Bad Request",
+            "test_get_product_trades_failure: Forbidden",
+            "test_get_product_trades_failure: Internal Server Error",
+        ],
+    )
+    def test_get_product_trades_failure(self, requests_mock, status_code):
+        requests_mock.get(self.URL + "/BTC-USD/trades", status_code=status_code)
+        with pytest.raises(HTTPError):
+            get_product_trades("BTC-USD")
+        assert requests_mock.called_once
+        assert requests_mock.last_request.url == self.URL + "/BTC-USD/trades"
         assert requests_mock.last_request.method == "GET"
         assert requests_mock.last_request.headers == HEADERS
